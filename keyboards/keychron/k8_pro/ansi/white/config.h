7018bd0be067542c30a0fe4b31edaeca82beb460
Ruben S <hi@scruben.com>
2023-01-04 17:48:35 +0100

    Initial layout and unicode experiments

--- keyboards/keychron/k8_pro/ansi/white/config.h
+++ keyboards/keychron/k8_pro/ansi/white/config.h
@@ -14,6 +14,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// #define TAP_CODE_DELAY 200
+#define TAPPING_TERM 267
+
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
+
 #ifdef LED_MATRIX_ENABLE
 /* LED matrix driver configuration */
 #    define DRIVER_COUNT 1
