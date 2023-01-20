fab1b8c60a1a0bcc2ba9e17f802867538b8c3786
Ruben S <hi@scruben.com>
2023-01-20 23:29:14 +0100

    keymap improvement to make it similar to currently used

--- keyboards/handwired/dactyl_manuform/5x6_r_track/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_r_track/keymaps/default/keymap.c
@@ -72,30 +72,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
     KC_GRV    , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                          KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,
     KC_TAB    , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
-    TD(LOW_TM), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
-    TD(SFT_TM), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT, KC_LSFT,
+    MO(_LOWER), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
+    KC_LSFT   , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT, KC_LSFT,
                         KC_LEFT, KC_RIGHT,                                                                                KC_UP  , KC_DOWN,
-                                        KC_BTN1, TD(RAI_TM),                                                   KC_SPC ,
-                                                            TD(GUI_TM), TD(ALT_TM),   KC_DEL ,          KC_ENT ,
-                                                            TD(CTL_TM), KC_ESC    ,            KC_BSPC
+                                        KC_BTN1, MO(_RAISE),                                                   KC_SPC ,
+                                                            KC_LGUI   , KC_LALT   ,   KC_DEL ,          KC_ENT ,
+                                                            KC_LCTL   , KC_ESC    ,            KC_BSPC
     ),
 
     [_LOWER] = LAYOUT_5x6(
-    KC_TILD   , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5   ,                                         KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10  , KC_F11, KC_F12 ,
-    KC_TAB    , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
-    KC_ESC    , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
-    TD(SFT_TM), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT, KC_LSFT,
+    KC_TILD   , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5   ,                                         KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11, KC_F12 ,
+    KC_TAB    , KC_Q   , KC_MNXT, KC_E   , KC_R   , KC_T   ,                                          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
+    KC_ESC    , KC_HOME, KC_MPLY, KC_BSPC, KC_DEL , KC_G   ,                                          KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_END, KC_QUOT, KC_ENT ,
+    KC_CAPS   , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT, KC_LSFT,
                         KC_LEFT, KC_RIGHT,                                                                                KC_UP  , KC_DOWN,
-                                        KC_BTN1, TD(RAI_TM),                                                   KC_SPC ,
+                                        KC_BTN1, TD(RAI_TM),                                                   KC_ENT,
                                                             TD(GUI_TM), TD(ALT_TM),   KC_DEL ,          KC_ENT ,
                                                             TD(CTL_TM), TD(LOW_TM),            KC_BSPC
     ),
 
     [_RAISE] = LAYOUT_5x6(
     KC_TILD   , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                          KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,
-    KC_TAB    , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
-    KC_ESC    , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
-    TD(SFT_TM), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT, KC_LSFT,
+    KC_TAB    , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                          KC_Y   , KC_1   , KC_2   , KC_3   , KC_P   , KC_LBRC, KC_RBRC,
+    KC_ESC    , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                          KC_H   , KC_4   , KC_5   , KC_6   , KC_0, KC_QUOT, KC_ENT ,
+    TD(SFT_TM), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                          KC_N   , KC_7   , KC_8   , KC_9   , KC_SLSH, KC_LSFT, KC_LSFT,
                         KC_LEFT, KC_RIGHT,                                                                                KC_UP  , KC_DOWN,
                                         KC_BTN1, TD(RAI_TM),                                                   KC_SPC ,
                                                             TD(GUI_TM), TD(ALT_TM),   KC_DEL ,          KC_ENT ,
