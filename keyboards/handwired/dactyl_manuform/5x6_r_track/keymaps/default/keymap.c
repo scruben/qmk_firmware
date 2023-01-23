c8c50aa3e975594eacac0f85b7d6f620dff41665
Ruben S <ruben@txodds.com>
2023-01-23 16:41:25 +0100

    Add volumen buttons

--- keyboards/handwired/dactyl_manuform/5x6_r_track/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_r_track/keymaps/default/keymap.c
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT_5x6(
-    KC_TILD   , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5   ,                                         KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11, KC_F12 ,
-    KC_TAB    , KC_Q   , KC_MNXT, KC_E   , KC_R   , KC_T   ,                                          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
-    KC_ESC    , KC_HOME, KC_MPLY, KC_DEL , KC_BSPC, KC_G   ,                                          KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_END, KC_QUOT, KC_ENT ,
+    KC_TILD   , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5   ,                                         KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11,  KC_F12 ,
+    KC_TAB    , KC_Q   , KC_MNXT, KC_VOLD, KC_VOLU, KC_T   ,                                          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
+    KC_ESC    , KC_HOME, KC_MPLY, KC_DEL , KC_BSPC, KC_G   ,                                          KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT,KC_END,  KC_QUOT, KC_ENT ,
     KC_LSFT   , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT, KC_LSFT,
                         KC_LEFT, KC_RIGHT,                                                                              KC_VOLU, KC_VOLD,
                                            KC_TAB , KC_CAPS,                                                   KC_ENT,
