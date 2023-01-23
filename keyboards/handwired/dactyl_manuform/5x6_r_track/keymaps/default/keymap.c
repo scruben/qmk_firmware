1d2817723bd9d3a9000a74e4d4a052911ffaee9b
Ruben S <ruben@txodds.com>
2023-01-23 17:49:51 +0100

    Change again ctrl position

--- keyboards/handwired/dactyl_manuform/5x6_r_track/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_r_track/keymaps/default/keymap.c
@@ -75,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MO(_LOWER), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
     KC_LSFT   , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT, KC_LSFT,
                         KC_LEFT, KC_RIGHT,                                                                              KC_UP  , KC_DOWN,
-                                           LGUI_T(KC_BSPC), MO(_RAISE),                                         KC_SPC ,
-                                                            RCTL_T(KC_ENT) , KC_LCTL,  KC_DEL ,         KC_ENT ,
+                                           LGUI_T(KC_BSPC), RCTL_T(KC_ENT),                                    KC_SPC ,
+                                                            MO(_RAISE)     , KC_LCTL, KC_DEL ,        KC_ENT ,
                                                             LALT_T(KC_DEL) , KC_ESC ,         KC_BSPC
     ),
 
