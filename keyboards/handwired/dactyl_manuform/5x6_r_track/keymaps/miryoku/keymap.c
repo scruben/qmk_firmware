5363e12981874c0202467aa05c9f0dee9d8cfcba
Ruben S <hi@scruben.com>
2023-02-07 23:36:10 +0100

    Set miryoku options initial

--- keyboards/handwired/dactyl_manuform/5x6_r_track/keymaps/miryoku/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_r_track/keymaps/miryoku/keymap.c
@@ -30,15 +30,52 @@
 #define _LOWER 1
 #define _RAISE 2
 
+// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+// [_QWERTY] = LAYOUT_5x6(
+//      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+//      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
+//      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+//      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+//                     KC_LBRC, KC_RBRC,                                                       KC_PLUS, KC_EQL,
+//                                        TD(GUI_TM),    TD(SFT_TM),        _______, KC_RSFT,
+//                                        TD(CTL_TM),    KC_SPC,            _______, KC_BSPC,
+//                                        TD(ALT_TM),    TD(LOW_TM),        KC_ENT, KC_RALT
+// ),
+
+// [_LOWER] = LAYOUT_5x6(
+
+// KC_TILD,     KC_F1,    KC_F2,    KC_F3,         KC_F4,     KC_F5,        KC_F6,   KC_F7,     KC_F8,     KC_F9,       KC_F10,     KC_DEL ,
+// _______,    _______, _______,  _______,       _______,   KC_LCBR,        KC_RCBR, KC_BTN1,   _______,   _______  ,   _______ ,   _______,
+// _______,    _______, _______,  TD(RAI_TM),     KC_DEL,   KC_LPRN,        KC_RPRN, KC_LEFT ,  KC_UP,     KC_DOWN,     KC_RGHT,    KC_PIPE,
+// KC_CAPS ,   _______, _______,  LCTL(KC_X), LCTL(KC_C),LCTL(KC_V),        _______, KC_BTN2  , _______,   _______,     _______,    _______,
+//                     _______,_______,                                                                  _______,_______,
+//                                         _______,_______,                 _______,_______,
+//                                         _______,_______,                 _______,_______,
+//                                         _______,_______,                 _______,_______
+// ),
+
+// [_RAISE] = LAYOUT_5x6(
+
+// _______, _______, _______, _______, _______, _______,                    _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_CALC,
+// _______, _______, _______, _______, _______, KC_LBRC,                    KC_RBRC, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_MUTE,
+// _______, _______, _______, _______, _______, KC_LPRN,                    KC_RPRN, KC_P4,   KC_P5,   KC_P6,   _______, KC_VOLU,
+// _______, _______, _______, _______, _______, _______,                    KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PEQL, KC_VOLD,
+//                              _______,_______,                                KC_DOT, KC_COMM,
+//                                         _______,_______,             _______,_______,
+//                                         _______,_______,             _______,_______,
+//                                         _______,_______,             _______,_______
+// ),
+// };
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT_5x6(
     KC_GRV    , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                          KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,
     KC_TAB    , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
-    MO(_LOWER), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_BSLS,
+    LT(_LOWER,KC_TAB),LSFT_T(KC_A),LALT_T(KC_S),LGUI_T(KC_D), LCTL_T(KC_F), LT(_RAISE,KC_G),          LT(_RAISE,KC_H),RCTL_T(KC_J), RGUI_T(KC_K), LALT_T(KC_L),RSFT_T(KC_SCLN), LT(_LOWER,KC_QUOT), KC_BSLS,
     KC_LSFT   , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT, KC_LSFT,
                          LSG(KC_TAB), LGUI(KC_LEFT),                                                                LGUI(KC_RGHT), LGUI(KC_TAB),
-                                           LGUI_T(KC_BSPC), RCTL_T(KC_ENT),                                    KC_SPC ,
+                                           LGUI_T(KC_BSPC), RCTL_T(KC_ENT),                                    LT(_RAISE,KC_SPC) ,
                                                             RALT_T(KC_BTN1), KC_DEL , KC_DEL,         KC_ENT ,
                                                             LSFT_T(KC_ESC) , KC_TAB ,         KC_BSPC
     ),
@@ -56,12 +93,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_5x6(
     KC_TILD   , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                          KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,
-    KC_TAB    , KC_Q   , KC_W   , KC_E   , KC_LCBR, KC_RCBR,                                          KC_Y   , KC_1   , KC_2   , KC_3   , KC_MINS, KC_LBRC, KC_RBRC,
-    KC_CAPS   , KC_A   , KC_S   , KC_DEL , KC_LPRN, KC_RPRN,                                          KC_H   , KC_4   , KC_5   , KC_6   , KC_0   , KC_QUOT, KC_ENT ,
-    KC_LSFT   , KC_Z   , KC_X   , KC_C   , KC_LBRC, KC_RBRC,                                          KC_N   , KC_7   , KC_8   , KC_9   , KC_EQL , KC_LSFT, KC_LSFT,
-                        KC_LEFT, KC_RIGHT,                                                                                KC_UP  , KC_DOWN,
+    KC_TAB    , KC_UNDS, KC_AMPR, KC_ASTR, KC_LCBR, KC_LCBR,                                          KC_RCBR, KC_7   , KC_8   , KC_9   , KC_MINS, KC_LBRC, KC_RBRC,
+    KC_CAPS   , KC_A   , KC_DLR , KC_PERC, KC_CIRC, KC_LPRN,                                          KC_RPRN, KC_4   , KC_5   , KC_6   , KC_0   , KC_QUOT, KC_ENT ,
+    KC_LSFT   , KC_PLUS, KC_EXLM, KC_AT  , KC_HASH, KC_LBRC,                                          KC_RBRC, KC_1   , KC_2   , KC_3   , KC_EQL , KC_LSFT, KC_LSFT,
+                         KC_LEFT, KC_RIGHT,                                                                                KC_UP  , KC_DOWN,
                                         KC_BTN1, TD(RAI_TM),                                                   KC_BSPC ,
-                                                            TD(GUI_TM), TD(ALT_TM),   KC_DEL ,          KC_ENT ,
-                                                            TD(CTL_TM), TD(LOW_TM),            KC_BSPC
+                                                            TD(GUI_TM), TD(ALT_TM),   KC_DEL,         KC_ENT ,
+                                                            TD(CTL_TM), TD(LOW_TM),           KC_BSPC
     )
 };
